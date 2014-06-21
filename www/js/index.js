0c5dd927914bc72e4faaba5e6061b9b59f0bd691
dastagirkhan <mohammed.dastagirkhan@gmail.com>
2014-06-21 03:44:11 +0200

    device plugin is added

--- www/js/index.js
+++ www/js/index.js
@@ -44,6 +44,11 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        var element = document.getElementById('deviceProperties');
+        element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
     }
 };
